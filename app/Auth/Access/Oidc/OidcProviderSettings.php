f7b69222931f435f4bcce2c1163769bc09b28157
Daniel Castro <danicc097@gmail.com>
2023-02-21 09:38:26 +0000

    dont verify calls and allow http

--- app/Auth/Access/Oidc/OidcProviderSettings.php
+++ app/Auth/Access/Oidc/OidcProviderSettings.php
@@ -15,169 +15,169 @@ use Psr\Http\Client\ClientInterface;
  */
 class OidcProviderSettings
 {
-    public string $issuer;
-    public string $clientId;
-    public string $clientSecret;
-    public ?string $redirectUri;
-    public ?string $authorizationEndpoint;
-    public ?string $tokenEndpoint;
+  public string $issuer;
+  public string $clientId;
+  public string $clientSecret;
+  public ?string $redirectUri;
+  public ?string $authorizationEndpoint;
+  public ?string $tokenEndpoint;
 
-    /**
-     * @var string[]|array[]
-     */
-    public ?array $keys = [];
+  /**
+   * @var string[]|array[]
+   */
+  public ?array $keys = [];
 
-    public function __construct(array $settings)
-    {
-        $this->applySettingsFromArray($settings);
-        $this->validateInitial();
+  public function __construct(array $settings)
+  {
+    $this->applySettingsFromArray($settings);
+    $this->validateInitial();
+  }
+
+  /**
+   * Apply an array of settings to populate setting properties within this class.
+   */
+  protected function applySettingsFromArray(array $settingsArray)
+  {
+    foreach ($settingsArray as $key => $value) {
+      if (property_exists($this, $key)) {
+        $this->$key = $value;
+      }
+    }
+  }
+
+  /**
+   * Validate any core, required properties have been set.
+   *
+   * @throws InvalidArgumentException
+   */
+  protected function validateInitial()
+  {
+    $required = ['clientId', 'clientSecret', 'redirectUri', 'issuer'];
+    foreach ($required as $prop) {
+      if (empty($this->$prop)) {
+        throw new InvalidArgumentException("Missing required configuration \"{$prop}\" value");
+      }
     }
 
-    /**
-     * Apply an array of settings to populate setting properties within this class.
-     */
-    protected function applySettingsFromArray(array $settingsArray)
-    {
-        foreach ($settingsArray as $key => $value) {
-            if (property_exists($this, $key)) {
-                $this->$key = $value;
-            }
-        }
+    // if (strpos($this->issuer, 'https://') !== 0) {
+    //     throw new InvalidArgumentException('Issuer value must start with https://');
+    // }
+  }
+
+  /**
+   * Perform a full validation on these settings.
+   *
+   * @throws InvalidArgumentException
+   */
+  public function validate(): void
+  {
+    $this->validateInitial();
+    $required = ['keys', 'tokenEndpoint', 'authorizationEndpoint'];
+    foreach ($required as $prop) {
+      if (empty($this->$prop)) {
+        throw new InvalidArgumentException("Missing required configuration \"{$prop}\" value");
+      }
+    }
+  }
+
+  /**
+   * Discover and autoload settings from the configured issuer.
+   *
+   * @throws OidcIssuerDiscoveryException
+   */
+  public function discoverFromIssuer(ClientInterface $httpClient, Repository $cache, int $cacheMinutes)
+  {
+    try {
+      $cacheKey = 'oidc-discovery::' . $this->issuer;
+      $discoveredSettings = $cache->remember($cacheKey, $cacheMinutes * 60, function () use ($httpClient) {
+        return $this->loadSettingsFromIssuerDiscovery($httpClient);
+      });
+      $this->applySettingsFromArray($discoveredSettings);
+    } catch (ClientExceptionInterface $exception) {
+      throw new OidcIssuerDiscoveryException("HTTP request failed during discovery with error: {$exception->getMessage()}");
+    }
+  }
+
+  /**
+   * @throws OidcIssuerDiscoveryException
+   * @throws ClientExceptionInterface
+   */
+  protected function loadSettingsFromIssuerDiscovery(ClientInterface $httpClient): array
+  {
+    $issuerUrl = rtrim($this->issuer, '/') . '/.well-known/openid-configuration';
+    $request = new Request('GET', $issuerUrl);
+    $response = $httpClient->sendRequest($request);
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    if (empty($result) || !is_array($result)) {
+      throw new OidcIssuerDiscoveryException("Error discovering provider settings from issuer at URL {$issuerUrl}");
     }
 
-    /**
-     * Validate any core, required properties have been set.
-     *
-     * @throws InvalidArgumentException
-     */
-    protected function validateInitial()
-    {
-        $required = ['clientId', 'clientSecret', 'redirectUri', 'issuer'];
-        foreach ($required as $prop) {
-            if (empty($this->$prop)) {
-                throw new InvalidArgumentException("Missing required configuration \"{$prop}\" value");
-            }
-        }
-
-        if (strpos($this->issuer, 'https://') !== 0) {
-            throw new InvalidArgumentException('Issuer value must start with https://');
-        }
+    if (!str_contains($this->issuer, $result['issuer']) && !str_contains($this->issuer, 'http://')) {
+      throw new OidcIssuerDiscoveryException('Unexpected issuer value found on discovery response');
     }
 
-    /**
-     * Perform a full validation on these settings.
-     *
-     * @throws InvalidArgumentException
-     */
-    public function validate(): void
-    {
-        $this->validateInitial();
-        $required = ['keys', 'tokenEndpoint', 'authorizationEndpoint'];
-        foreach ($required as $prop) {
-            if (empty($this->$prop)) {
-                throw new InvalidArgumentException("Missing required configuration \"{$prop}\" value");
-            }
-        }
+    $discoveredSettings = [];
+
+    if (!empty($result['authorization_endpoint'])) {
+      $discoveredSettings['authorizationEndpoint'] = $result['authorization_endpoint'];
     }
 
-    /**
-     * Discover and autoload settings from the configured issuer.
-     *
-     * @throws OidcIssuerDiscoveryException
-     */
-    public function discoverFromIssuer(ClientInterface $httpClient, Repository $cache, int $cacheMinutes)
-    {
-        try {
-            $cacheKey = 'oidc-discovery::' . $this->issuer;
-            $discoveredSettings = $cache->remember($cacheKey, $cacheMinutes * 60, function () use ($httpClient) {
-                return $this->loadSettingsFromIssuerDiscovery($httpClient);
-            });
-            $this->applySettingsFromArray($discoveredSettings);
-        } catch (ClientExceptionInterface $exception) {
-            throw new OidcIssuerDiscoveryException("HTTP request failed during discovery with error: {$exception->getMessage()}");
-        }
+    if (!empty($result['token_endpoint'])) {
+      $discoveredSettings['tokenEndpoint'] = $result['token_endpoint'];
     }
 
-    /**
-     * @throws OidcIssuerDiscoveryException
-     * @throws ClientExceptionInterface
-     */
-    protected function loadSettingsFromIssuerDiscovery(ClientInterface $httpClient): array
-    {
-        $issuerUrl = rtrim($this->issuer, '/') . '/.well-known/openid-configuration';
-        $request = new Request('GET', $issuerUrl);
-        $response = $httpClient->sendRequest($request);
-        $result = json_decode($response->getBody()->getContents(), true);
-
-        if (empty($result) || !is_array($result)) {
-            throw new OidcIssuerDiscoveryException("Error discovering provider settings from issuer at URL {$issuerUrl}");
-        }
-
-        if (!str_contains($this->issuer, $result['issuer']) && !str_contains($this->issuer, 'http://')) {
-            throw new OidcIssuerDiscoveryException('Unexpected issuer value found on discovery response');
-        }
-
-        $discoveredSettings = [];
-
-        if (!empty($result['authorization_endpoint'])) {
-            $discoveredSettings['authorizationEndpoint'] = $result['authorization_endpoint'];
-        }
-
-        if (!empty($result['token_endpoint'])) {
-            $discoveredSettings['tokenEndpoint'] = $result['token_endpoint'];
-        }
-
-        if (!empty($result['jwks_uri'])) {
-            $keys = $this->loadKeysFromUri($result['jwks_uri'], $httpClient);
-            $discoveredSettings['keys'] = $this->filterKeys($keys);
-        }
-
-        return $discoveredSettings;
+    if (!empty($result['jwks_uri'])) {
+      $keys = $this->loadKeysFromUri($result['jwks_uri'], $httpClient);
+      $discoveredSettings['keys'] = $this->filterKeys($keys);
     }
 
-    /**
-     * Filter the given JWK keys down to just those we support.
-     */
-    protected function filterKeys(array $keys): array
-    {
-        return array_filter($keys, function (array $key) {
-            $alg = $key['alg'] ?? 'RS256';
-            $use = $key['use'] ?? 'sig';
+    return $discoveredSettings;
+  }
 
-            return $key['kty'] === 'RSA' && $use === 'sig' && $alg === 'RS256';
-        });
+  /**
+   * Filter the given JWK keys down to just those we support.
+   */
+  protected function filterKeys(array $keys): array
+  {
+    return array_filter($keys, function (array $key) {
+      $alg = $key['alg'] ?? 'RS256';
+      $use = $key['use'] ?? 'sig';
+
+      return $key['kty'] === 'RSA' && $use === 'sig' && $alg === 'RS256';
+    });
+  }
+
+  /**
+   * Return an array of jwks as PHP key=>value arrays.
+   *
+   * @throws ClientExceptionInterface
+   * @throws OidcIssuerDiscoveryException
+   */
+  protected function loadKeysFromUri(string $uri, ClientInterface $httpClient): array
+  {
+    $request = new Request('GET', $uri);
+    $response = $httpClient->sendRequest($request);
+    $result = json_decode($response->getBody()->getContents(), true);
+
+    if (empty($result) || !is_array($result) || !isset($result['keys'])) {
+      throw new OidcIssuerDiscoveryException('Error reading keys from issuer jwks_uri');
     }
 
-    /**
-     * Return an array of jwks as PHP key=>value arrays.
-     *
-     * @throws ClientExceptionInterface
-     * @throws OidcIssuerDiscoveryException
-     */
-    protected function loadKeysFromUri(string $uri, ClientInterface $httpClient): array
-    {
-        $request = new Request('GET', $uri);
-        $response = $httpClient->sendRequest($request);
-        $result = json_decode($response->getBody()->getContents(), true);
+    return $result['keys'];
+  }
 
-        if (empty($result) || !is_array($result) || !isset($result['keys'])) {
-            throw new OidcIssuerDiscoveryException('Error reading keys from issuer jwks_uri');
-        }
-
-        return $result['keys'];
+  /**
+   * Get the settings needed by an OAuth provider, as a key=>value array.
+   */
+  public function arrayForProvider(): array
+  {
+    $settingKeys = ['clientId', 'clientSecret', 'redirectUri', 'authorizationEndpoint', 'tokenEndpoint'];
+    $settings = [];
+    foreach ($settingKeys as $setting) {
+      $settings[$setting] = $this->$setting;
     }
 
-    /**
-     * Get the settings needed by an OAuth provider, as a key=>value array.
-     */
-    public function arrayForProvider(): array
-    {
-        $settingKeys = ['clientId', 'clientSecret', 'redirectUri', 'authorizationEndpoint', 'tokenEndpoint'];
-        $settings = [];
-        foreach ($settingKeys as $setting) {
-            $settings[$setting] = $this->$setting;
-        }
-
-        return $settings;
-    }
+    return $settings;
+  }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,63 +21,64 @@ use Whoops\Handler\HandlerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Custom container bindings to register.
-     * @var string[]
-     */
-    public $bindings = [
-        HandlerInterface::class => WhoopsBookStackPrettyHandler::class,
-    ];
+  /**
+   * Custom container bindings to register.
+   * @var string[]
+   */
+  public $bindings = [
+    HandlerInterface::class => WhoopsBookStackPrettyHandler::class,
+  ];
 
-    /**
-     * Custom singleton bindings to register.
-     * @var string[]
-     */
-    public $singletons = [
-        'activity' => ActivityLogger::class,
-        SettingService::class => SettingService::class,
-        SocialAuthService::class => SocialAuthService::class,
-        CspService::class => CspService::class,
-    ];
+  /**
+   * Custom singleton bindings to register.
+   * @var string[]
+   */
+  public $singletons = [
+    'activity' => ActivityLogger::class,
+    SettingService::class => SettingService::class,
+    SocialAuthService::class => SocialAuthService::class,
+    CspService::class => CspService::class,
+  ];
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // Set root URL
-        $appUrl = config('app.url');
-        if ($appUrl) {
-            $isHttps = (strpos($appUrl, 'https://') === 0);
-            URL::forceRootUrl($appUrl);
-            URL::forceScheme($isHttps ? 'https' : 'http');
-        }
-
-        // Allow longer string lengths after upgrade to utf8mb4
-        Schema::defaultStringLength(191);
-
-        // Set morph-map for our relations to friendlier aliases
-        Relation::enforceMorphMap([
-            'bookshelf' => Bookshelf::class,
-            'book'      => Book::class,
-            'chapter'   => Chapter::class,
-            'page'      => Page::class,
-        ]);
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    // Set root URL
+    $appUrl = config('app.url');
+    if ($appUrl) {
+      $isHttps = (strpos($appUrl, 'https://') === 0);
+      URL::forceRootUrl($appUrl);
+      URL::forceScheme($isHttps ? 'https' : 'http');
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind(HttpClientInterface::class, function ($app) {
-            return new Client([
-                'timeout' => 3,
-            ]);
-        });
-    }
+    // Allow longer string lengths after upgrade to utf8mb4
+    Schema::defaultStringLength(191);
+
+    // Set morph-map for our relations to friendlier aliases
+    Relation::enforceMorphMap([
+      'bookshelf' => Bookshelf::class,
+      'book'      => Book::class,
+      'chapter'   => Chapter::class,
+      'page'      => Page::class,
+    ]);
+  }
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->app->bind(HttpClientInterface::class, function ($app) {
+      return new Client([
+        'timeout' => 6,
+        'verify' => false,
+      ]);
+    });
+  }
 }
